74013a05a86a53bcc8e7a6468a9a077f81d31b2e
bhavyagalav <skmsirkadash@gmail.com>
2026-06-12 18:35:24 +0530

    payment system  updated

--- js/booking.js
+++ js/booking.js
@@ -2,7 +2,7 @@
 // SHIKSHA LIBRARY — BOOKING + PAYMENT SYSTEM v2
 // ============================================================
 
-const RAZORPAY_KEY_ID   = "rzp_test_T0icB6lw0FyPy8";
+const RAZORPAY_KEY_ID   = "rzp_live_T0jBbWu8yxxkgp";
 const SUPABASE_URL = "https://ojmxqckunnpthgftwbkq.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_j8mlu6_1po8jUo3k5tpMrw_l7Wq74VS";
 
